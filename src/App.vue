87da0dd020f173cd8e9754d66d543d466f104d4b
Sceat <11330271+Sceat@users.noreply.github.com>
2019-01-05 19:07:37 +0200

    starting meta feature

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,50 @@
 
 <script>
 import { Vue, Component } from 'vue-property-decorator'
+import config from '@root/hydre.toml'
 
 @Component({
   components: {
     utils: () => import('@cmp/utils/index.vue'),
     loading: () => import('@cmp/utils/loading.vue')
+  },
+
+  metas() {
+    const configLang = config[this.$i18n.locale]
+
+    const og = [
+      { property: 'og:description', content: configLang.description },
+      { property: 'og:image', content: config.ogImage }
+    ]
+
+    const tw = [
+      { property: 'twitter:description', content: configLang.description },
+      { property: 'twitter:image', content: config.ogImage }
+    ]
+
+    config.og.forEach(meta => og.push(meta))
+    config.tw.forEach(meta => tw.push(meta))
+
+    return {
+      title: conf.title,
+      titleTemplate: eval(conf.titleTemplate),
+      meta: [
+        { name: 'description', content: configLang.description },
+        { property: 'og:title', content: configLang.ogTitle, template: configLang.ogTemplate },
+        { name: 'msapplication-TileColor', content: config.tileColor },
+        { name: 'theme-color', content: config.themeColor },
+        ...og,
+        ...tw
+      ],
+      link: [
+        { rel: 'apple-touch-icon', sizes: config.touchIconSize, href: '<%= BASE_URL %>img/icons/apple-touch-icon.png' },
+        { rel: 'mask-icon', href: '<%= BASE_URL %>img/icons/safari-pinned-tab.svg', color: config.maskIconColor }
+      ]
+    }
   }
 })
-export default class App extends Vue { }
+export default class App extends Vue {
+}
 </script>
 
 <style lang="stylus">
@@ -26,13 +62,25 @@ export default class App extends Vue { }
 
 *
   sc-reset()
-  sc-disableScollBar()
   font-family 'Open Sans', sans-serif
   outline none
   scroll-behavior smooth
+
+  &::-webkit-scrollbar-track
+    box-shadow inset 0 0 6px rgba(0, 0, 0, .3)
+    background-color #555
+
+  &::-webkit-scrollbar
+    width 12px
+    background-color #F5F5F5
+
+  &::-webkit-scrollbar-thumb
+    box-shadow inset 0 0 6px rgba(0, 0, 0, .3)
+    background-color #252525
 
   a
     :active
       color #e1c79b
       fill #e1c79b
 </style>
+
