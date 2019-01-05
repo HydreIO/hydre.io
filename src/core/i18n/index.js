c549cb05e8f431ca4b9a2e44d041f38b687bc923
Sceat <11330271+Sceat@users.noreply.github.com>
2019-01-05 19:07:25 +0200

    fix i18n detection

--- src/core/i18n/index.js
+++ src/core/i18n/index.js
@@ -8,7 +8,7 @@ const messages = {
 }
 
 export default new VueI18n({
-	locale: localStorage.getItem('locale') || (navigator.languages ? navigator.languages[0] : navigator.language),
+	locale: localStorage.getItem('locale') || (navigator.languages ? navigator.languages[0] : navigator.language).substring(0,2),
 	fallbackLocale: 'en',
 	messages,
 	missing: (locale, key) => {
