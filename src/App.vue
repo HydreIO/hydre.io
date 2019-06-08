97632428ac97fba6131ded3f43baa3a5c4634d1b
Sceat <mr.sceat@outlook.com>
2019-06-08 21:09:54 +0300

    fix fonts

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,7 @@
 					...tw
 				],
 				link: [
+					{ rel: "stylesheet", href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' },
 					{ rel: 'apple-touch-icon', sizes: config.touchIconSize, href: '<%= BASE_URL %>img/icons/apple-touch-icon.png' },
 					{ rel: 'mask-icon', href: '<%= BASE_URL %>img/icons/safari-pinned-tab.svg', color: config.maskIconColor }
 				]
@@ -53,7 +54,6 @@
 
 <style lang="stylus">
 @require '~@stl/utils'
-@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800')
 
 :root
 	font-size 18px
